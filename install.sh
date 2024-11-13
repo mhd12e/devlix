2e9a442b8f82d11a30109335ffac60646c5f4531
mhd12 <mo212el4@gmail.com>
2024-11-13 20:56:52 +0400

    Fix installer

--- install.sh
+++ install.sh
@@ -224,7 +224,8 @@ echo "Building Devlix WM ..."
 echo -e "---------------------------------------------\n\n"
 sleep 2
 
-sed -i "s/mohamed/$(whoami)/g" ~/devlix/dwm/config.def.h ~/devlix/dwm/config.h
+sed -i "s/mohamed/$(whoami)/g" ~/devlix/dwm/config.def.h
+[ -f ~/devlix/dwm/config.h ] && sed -i "s/mohamed/$(whoami)/g" ~/devlix/dwm/config.h
 
 (cd ~/devlix/dwm && sudo make clean install)
 (cd ~/devlix/dmenu && sudo make clean install)
